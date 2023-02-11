57c34ecace40025c024e7bba12635d68f00321ea
Muhammad zulqarnain <zulqarnainniazi95@gmail.com>
2023-02-11 13:55:58 -0800

    completed the table component with just logo missing

--- components/Table.jsx
+++ components/Table.jsx
@@ -4,6 +4,17 @@ import TableItem from './TableItem';
 const Table = ({data}) => {
   return (
     <>
+    <div className='flex flex-col px-6  sm:px-10 lg:px-[100px] my-6 w-full'>
+<h1 className='font-bold text-2xl my-5'>Top Traded</h1>
+<div className='grid grid-cols-3 md:grid-cols-6 px-4 '>
+<p className='col-span-1 font-semibold text-indigo-500 '>#</p>
+<p className='col-span-1 font-semibold text-indigo-500 '>TOKEN</p>
+<p className='col-span-1 font-semibold text-indigo-500 '>VOLUME</p>
+<p className='col-span-1 hidden md:flex font-semibold text-indigo-500 '>PRICE</p>
+<p className='col-span-1 hidden md:flex font-semibold text-indigo-500 '>CHANGE(24H)</p>
+<p className='col-span-1 hidden md:flex font-semibold text-indigo-500 '>CHANGE(7D)</p>
+</div>
+   
     {
         data.map((item)=>{
             return (
@@ -13,12 +24,13 @@ const Table = ({data}) => {
                 logo={item.image}
                 volume={item.total_volume} 
                 price={item.current_price}
-                change24h={item.price_change_percentage_24h_in_currency} 
-                change7d={item.price_change_percentage_7d_in_currency}
+                change24h={item.price_change_percentage_24h_in_currency.toString().slice(0,4)} 
+                change7d={item.price_change_percentage_7d_in_currency.toString().slice(0,4)}
                 />
             )
         })
     }
+     </div>
     </>
   )
 }
--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['assets.coingecko.com/coins/images'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.coingecko.com',
+        port: '',
+        pathname: '/coins/images/',
+      },
+    ],
   },
 }
 
