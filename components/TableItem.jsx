721fe032ea234b7c18e8eb42f80f6f55e095bc1c
Muhammad zulqarnain <zulqarnainniazi95@gmail.com>
2023-02-11 19:29:34 -0800

    fixed table fro mobile view

--- components/TableItem.jsx
+++ components/TableItem.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const TableItem = ({rank,title,logo,volume,change24h,change7d,price}) => {
   return (
-    <div className='grid md:grid-cols-6 grid-cols-2  hover:bg-slate-200 cursor-pointer rounded-lg  p-4'>
+    <div className='grid gap-16 sm:gap-0 md:grid-cols-6 grid-cols-2  hover:bg-slate-200 cursor-pointer rounded-lg  p-4'>
       
-      <div className='md:col-span-2 flex gap-6 sm:gap-[2rem] md:gap-16 lg:gap-20'>
+      <div className='md:col-span-2 flex gap-4 sm:gap-[2rem] md:gap-16 lg:gap-20'>
       <p className='col-span-1'>{rank}</p>
         <div className='flex gap-2'>
       <Image className='h-6 w-6' src={logo} height='500' width='500' />
