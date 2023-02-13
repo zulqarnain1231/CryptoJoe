87e199ce811df14972e7f35eebffcc523b5021ae
Muhammad zulqarnain <zulqarnainniazi95@gmail.com>
2023-02-13 14:27:33 -0800

    fixed  the items component

--- components/Items.jsx
+++ components/Items.jsx
@@ -8,15 +8,15 @@ const Items = () => {
             <h1 className='my-3 font-bold text-xl'>Explore Trader Joe</h1>
             <div className='grid text-center grid-cols-2 md:grid-cols-3 '>
             <Slide direction='left'>
-                <Item img='/firstimage.webp' title='Trade' link='Trade' darkColor='green-400' color='green-200' border='green-500' />
-                <Item img='/secondimage.webp' title='Trade' darkColor='indigo-400' color='indigo-200' border='blue-600' />
-                <Item img='/thirdimage.webp' title='Trade' darkColor='orange-400' color='orange-200' border='orange-600' />
+                <Item img='/firstimage.webp' title='Trade' link='Trade' darkColor='bg-green-400' color='bg-green-200' border='hover:border-green-500' />
+                <Item img='/secondimage.webp' title='Trade' darkColor='bg-indigo-400' color='bg-indigo-200' border='hover:border-blue-600' />
+                <Item img='/thirdimage.webp' title='Trade' darkColor='bg-orange-400' color='bg-orange-200' border='hover:border-orange-600' />
                 </Slide>
                 <Slide direction='right'>
 
-                <Item img='/fourthimage.webp' title='Trade' darkColor='pink-400' color='pink-200' border='pink-600' />
-                <Item img='/fifthimage.webp' title='Trade' darkColor='cyan-400' color='cyan-200' border='cyan-600' />
-                <Item img='/sixthimage.webp' title='Trade' darkColor='purple-400' color='purple-200' border='purple-600' />
+                <Item img='/fourthimage.webp' title='Trade' darkColor='bg-pink-400' color='bg-pink-200' border='hover:border-pink-600' />
+                <Item img='/fifthimage.webp' title='Trade' darkColor='bg-cyan-400' color='bg-cyan-200' border='hover:border-cyan-600' />
+                <Item img='/sixthimage.webp' title='Trade' darkColor='bg-purple-400' color='bg-purple-200' border='hover:border-purple-600' />
                 </Slide>
             </div>
 
