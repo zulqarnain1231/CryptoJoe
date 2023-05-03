dcab78b821a0a19144d15fdb32345b6dae8b2c18
Muhammad zulqarnain <zulqarnainniazi447@gmail.com>
2023-05-03 06:50:51 -0700

    fixed hero section

--- components/Herosection.jsx
+++ components/Herosection.jsx
@@ -3,12 +3,12 @@ import mainImage from '../public/herosectionimg.webp'
 import Image from 'next/image'
 const Herosection = () => {
   return (
-    <div className='flex flex-col gap-4 text-center px-5 py-4 my-3 '>
-      <div className=' w-full items-center'>
-        <Image className='object-cover h-full mx-auto w-[80%] md:w-[60%]' src={mainImage} alt='heroimg' />
+    <div className='w-full flex flex-col gap-4 items-center justify-center px-5 py-4 my-3 '>
+      <div className=' w-[80%] h-[350px] relative'>
+        <Image className='' src={mainImage} alt='heroimg' fill />
 
       </div>
-      <div className='my-12'>
+      <div className='w-full flex items-center justify-center'>
         <h1 className='font-bold text-3xl md:text-4xl'>One-stop decentralized trading</h1>
       </div>
     </div>
--- components/Table.jsx
+++ components/Table.jsx
@@ -7,21 +7,22 @@ const Table = ({ data }) => {
             <div className='flex flex-col px-6  sm:px-10  lg:px-[80px] my-12 w-full'>
                 <h1 className='font-bold text-2xl my-5'>Top Traded</h1>
                 <div className='w-full overflow-auto HideScroll'>
-                <div className='grid  h-[40px]  grid-cols-[.5fr,1fr,1fr,1fr,1fr,1fr] px-4 min-w-[600px] w-full overflow-auto'>
-                    <p className=' flex items-center font-semibold text-indigo-500 '>#</p>
-                    <p className=' flex items-center font-semibold text-indigo-500 '>TOKEN</p>
-                    <p className=' flex items-center font-semibold text-indigo-500 '>VOLUME</p>
-                    <p className=' flex items-center   font-semibold text-indigo-500 '>PRICE</p>
-                    <p className=' flex items-center font-semibold text-indigo-500 '>CHANGE(24H)</p>
-                    <p className=' flex items-center font-semibold text-indigo-500 '>CHANGE(7D)</p>
+                    <div className='grid  h-[40px]  grid-cols-[.5fr,1fr,1fr,1fr,1fr,1fr] px-4 min-w-[600px] w-full overflow-auto'>
+                        <p className=' flex items-center font-semibold text-indigo-500 '>#</p>
+                        <p className=' flex items-center font-semibold text-indigo-500 '>TOKEN</p>
+                        <p className=' flex items-center font-semibold text-indigo-500 '>VOLUME</p>
+                        <p className=' flex items-center   font-semibold text-indigo-500 '>PRICE</p>
+                        <p className=' flex items-center font-semibold text-indigo-500 '>CHANGE(24H)</p>
+                        <p className=' flex items-center font-semibold text-indigo-500 '>CHANGE(7D)</p>
+                    </div>
                 </div>
-                </div>
-    
+
                 <div className='w-full min-w-[600px] h-[500px] flex flex-col overflow-auto ThinScrollbar'>
                     {
-                        data.map((item) => {
+                        data.map((item, index) => {
                             return (
                                 <TableItem
+                                    key={index}
                                     rank={item.market_cap_rank}
                                     title={item.symbol}
                                     logo={item.image}
