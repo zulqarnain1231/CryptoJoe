944da3bb7daafb53a4efdee2209016aa4da2ffd1
Muhammad zulqarnain <zulqarnainniazi447@gmail.com>
2023-05-03 01:50:30 -0700

    fixed minor issues in table

--- components/Table.jsx
+++ components/Table.jsx
@@ -6,11 +6,12 @@ const Table = ({ data }) => {
         <>
             <div className='flex flex-col px-6  sm:px-10  lg:px-[80px] my-12 w-full'>
                 <h1 className='font-bold text-2xl my-5'>Top Traded</h1>
-                <div className='grid  justify-center text-center sm:gap-0 grid-cols-2 md:grid-cols-6 px-4 w-full'>
-                    <div className='md:col-span-2  flex gap-4 sm:gap-[2rem] md:gap-16 lg:gap-20'>
-                        <p className=' font-semibold text-indigo-500 '>#</p>
-                        <p className=' font-semibold text-indigo-500 '>TOKEN</p>
-                    </div>
+                <div className='grid  sm:gap-0 grid-cols-2 md:grid-cols-[.5fr,1fr,1fr,1fr,1fr,1fr] px-4 w-full'>
+                    <p className=' font-semibold text-indigo-500 '>#</p>
+
+
+                    <p className=' font-semibold text-indigo-500 '>TOKEN</p>
+
                     <p className='col-span-1 hidden md:flex items-center font-semibold text-indigo-500 '>VOLUME</p>
                     <p className='col-span-1  font-semibold text-indigo-500 '>PRICE</p>
                     <p className='col-span-1 hidden md:flex items-center font-semibold text-indigo-500 '>CHANGE(24H)</p>
--- components/TableItem.jsx
+++ components/TableItem.jsx
@@ -8,19 +8,19 @@ const TableItem = ({ rank, title, logo, volume, change24h, change7d, price }) =>
 
 
   return (
-    <div className={`grid  justify-center text-center sm:gap-0 md:grid-cols-6 grid-cols-2 ${darkMode ? 'hover:bg-slate-800' : 'hover:bg-slate-200 '}  cursor-pointer rounded-lg  p-4`}>
+    <div className={`w-full grid   sm:gap-0 md:grid-cols-[.5fr,1fr,1fr,1fr,1fr,1fr] grid-cols-2 ${darkMode ? 'hover:bg-slate-800' : 'hover:bg-slate-200 '}  cursor-pointer rounded-lg  p-4`}>
+      <p className=''>{rank}</p>
+      <div className=' flex items-center gap-4 sm:gap-[2rem] md:gap-16 lg:gap-20'>
 
-      <div className='md:col-span-2 flex items-center gap-4 sm:gap-[2rem] md:gap-16 lg:gap-20'>
-        <p className='col-span-1'>{rank}</p>
         <div className='flex items-center gap-2'>
           <Image className='h-6 w-6' src={logo} height='500' width='500' />
           <p className='font-bold uppercase'>{title}</p>
         </div>
       </div>
-      <p className='font-bold hidden md:flex items-center'>${volume.toLocaleString()}</p>
-      <p className='font-bold  '>${price.toLocaleString()}</p>
-      <p className={`${change24h < 0 ? 'text-red-500' : 'text-green-600'} font-bold hidden md:flex items-center`}>{change24h}%</p>
-      <p className={`${change7d < 0 ? 'text-red-500' : 'text-green-600'} font-bold hidden md:flex items-center`}>{change7d}%</p>
+      <p className=' w-full font-bold hidden md:flex items-center'>${volume.toLocaleString()}</p>
+      <p className=' w-full font-bold  '>${price.toLocaleString()}</p>
+      <p className={`${change24h < 0 ? 'text-red-500' : 'text-green-600'} w-full  font-bold hidden md:flex items-center`}>{change24h}%</p>
+      <p className={`${change7d < 0 ? 'text-red-500' : 'text-green-600'} w-full  font-bold hidden md:flex items-center`}>{change7d}%</p>
     </div>
   )
 }
