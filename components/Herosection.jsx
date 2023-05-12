33759cd527ea541841ea55fe62e5727d379da753
Muhammad zulqarnain <zulqarnainniazi447@gmail.com>
2023-05-12 04:27:47 -0700

    fixed the hero section

--- components/Herosection.jsx
+++ components/Herosection.jsx
@@ -3,13 +3,13 @@ import mainImage from '../public/herosectionimg.webp'
 import Image from 'next/image'
 const Herosection = () => {
   return (
-    <div className='w-full flex flex-col gap-4 items-center justify-center px-5 py-4 my-3 '>
-      <div className=' w-[80%] h-[350px] relative'>
-        <Image className='' src={mainImage} alt='heroimg' fill />
+    <div className='w-full flex flex-col gap-4 items-center justify-center text-center px-5 py-4 my-3 '>
+      <div className=' w-[80%] sm:h-[350px] h-[250px] relative'>
+        <Image className='bg-cover' src={mainImage} alt='heroimg' fill />
 
       </div>
       <div className='w-full flex items-center justify-center'>
-        <h1 className='font-bold text-3xl md:text-4xl'>One-stop decentralized trading</h1>
+        <h1 className='font-bold text-2xl  sm:text-3xl md:text-4xl'>One-stop decentralized trading</h1>
       </div>
     </div>
   )
